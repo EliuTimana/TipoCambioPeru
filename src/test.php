3a2d8023564c327a53052509782c66da027df9ce
Eliú Timaná <EliuTimana@gmail.com>
2018-11-10 20:26:50 -0500

    eliminado comentarios

--- src/test.php
+++ src/test.php
@@ -7,26 +7,6 @@ require '../vendor/autoload.php';
  * Date: 03/14/2018
  * Time: 14:33.
  */
-/*$content = file_get_contents('http://e-consulta.sunat.gob.pe/cl-at-ittipcam/tcS01Alias');
-libxml_use_internal_errors(true);
-if (!empty($content)) {
-    $domDocument = new DOMDocument();
-    $domDocument->loadHTML($content);
-
-    $domXPath = new DOMXPath($domDocument);
-
-    $nodesTr = $domXPath->query('(//table[contains(@class, "form-table")])[1]/tr');
-    foreach ($nodesTr as $r => $tr) {
-        if ($r > 0) {
-            $nodesTd = $tr->getElementsByTagName('td');
-            for ($i = 0; $i < $nodesTd->length; $i += 3) {
-                echo 'dia:'.$nodesTd->item($i)->nodeValue.'<br>';
-                echo 'compa: '.$nodesTd->item($i + 1)->nodeValue.'<br>';
-                echo 'venta:'.$nodesTd->item($i + 2)->nodeValue.'<br>';
-            }
-        }
-    }
-}*/
 
 $tipocambio = new \EliuTimana\TipoCambioPeru\TipoCambio();
 print_r($tipocambio->getCambioMes());
