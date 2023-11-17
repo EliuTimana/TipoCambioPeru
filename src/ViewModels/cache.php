49f24e1c9efddfce19e56937ad4a0a1dec4d6f6b
Eliú Timaná <EliuTimana@gmail.com>
2023-11-17 09:18:01 -0500

    🚀 RELEASE: "[HttpKernel] Fix advertizing deprecations for *TestSessionListener"

--- src/ViewModels/cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require '../vendor/autoload.php';
-
-$tipocambio = new \EliuTimana\TipoCambioPeru\TipoCambio();
-echo $tipocambio->getCambioMes();
-echo $tipocambio->getCambioFecha(new DateTime('2017-08-06'), true);
-echo $tipocambio->getCambioHoy();
