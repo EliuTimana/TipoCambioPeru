427c5c036b205f507e9f3faa5469bd2bc678cdf5
Eliú Timaná <EliuTimana@gmail.com>
2025-05-13 19:21:01 -0500

    🛠️ REFACTOR: "Merge branch '4.4'"

--- src/naked.php
+++ src/naked.php
@@ -3,6 +3,4 @@
 require '../vendor/autoload.php';
 
 $tipocambio = new \EliuTimana\TipoCambioPeru\TipoCambio();
-echo $tipocambio->getCambioMes();
 echo $tipocambio->getCambioFecha(new DateTime('2017-08-06'), true);
-echo $tipocambio->getCambioHoy();
